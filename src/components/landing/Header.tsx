fadcd1be4d7f66a4210d05a9b35e2222036f8ddc
Walysson Portela <walyssonrodrigues833@gmail.com>
2025-10-23 20:36:57 +0000

    change English for color #00247d, and Faster for #cf1b2b. After centrali

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export function Header() {
   return (
     <header className="py-4 px-4 md:px-8 bg-card border-b sticky top-0 z-40 shadow-sm">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-center">
         <Link href="/" className="flex items-center gap-2 w-fit">
           <h1 className="text-2xl font-bold font-headline">
-            EnglishFaster
+            <span style={{ color: '#00247d' }}>English</span><span style={{ color: '#cf1b2b' }}>Faster</span>
           </h1>
         </Link>
       </div>
